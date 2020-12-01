a24fe354b98c725876a3ccbfc49055f0cd208a01
Jianghuil <74592402+Jianghuil@users.noreply.github.com>
2020-12-01 17:40:51 +0800

    Update StudentModel.java

--- student_prc/src/com/student/model/StudentModel.java
+++ student_prc/src/com/student/model/StudentModel.java
@@ -5,16 +5,12 @@ public class StudentModel {
 	private String username;
 	private String password;
 	private int sex;
-	private String love;
 	
-	public StudentModel(int id,String username,String password,int sex,String love) {
+	public StudentModel(int id,String username,String password,int sex) {
 		super();
 		this.id=id;
 		this.username=username;
 		this.password=password;
 		this.sex=sex;
-		this.love=love;
-	}
-	
-
+	}	
 }
